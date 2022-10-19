81060637bd594089520d6dfa06cc3c51b7dde4df
tuncayofficial <qtuncay70@gmail.com>
2022-10-20 00:04:01 +0400

    adding main file

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Main = (props)=>{
     return(
-        <h1>Hello world!!!</h1>
+        <div align = "center">
+            <h1 style = {{ marginTop : 50 }}>JS Math</h1>
+        </div>
     )
 }
 
